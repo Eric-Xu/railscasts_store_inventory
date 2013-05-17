2b4cb5199e0d12d131ab9cf47901ed37ea6554e6
Eric Xu <e.xulander@gmail.com>
2013-05-16 22:15:30 -0700

    add checkboxes for a many-to-many relationship from ep17

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :price_in_cents, :price_in_dollars, :released_at, :released_at_text, :category_id, :new_category, :tag_names
+  attr_accessible :name, :price_in_cents, :price_in_dollars, :released_at, :released_at_text, :category_id, :new_category, :tag_names, :age_group_ids
   belongs_to :category
   has_many :taggings
   has_many :tags, through: :taggings
